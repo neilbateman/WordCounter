117d25c2240b4cb6090da59862cba21801777bce
neilbateman <46337141+neilbateman@users.noreply.github.com>
2019-04-26 16:24:12 -0700

    update indentation, add boilerplate logic

--- WordCounter.Solution/WordCounter/Models/RepeatCounter.cs
+++ WordCounter.Solution/WordCounter/Models/RepeatCounter.cs
@@ -1,47 +1,49 @@
+using System;
+using System.Collections.Generic;
 namespace WordCounter.Models
 {
-  public class RepeatCounter
-  {
-    private string _intakeSentence;
-    private string _intakeWord;
+    public class RepeatCounter
+    {
+        private string _intakeSentence;
+        private string _intakeWord;
 
-    public RepeatCounter(string sentence, string word)
-    {
-        _intakeSentence = sentence.ToLower();
-        _intakeWord = word.ToLower();
-    }
-    public string GetSentence()
-    {
-        return _intakeSentence;
-    }
-    public string GetWord()
-    {
-        return _intakeWord;
-    }
-    public void SetSentence(string setSentence)
-    {
-        _intakeSentence = setSentence;
-    }
-    public void SetWord(string setWord)
-    {
-        _intakeWord = setWord;
-    }
-
-    public int GetPairs()
-    {
-        int pairs = 0;
-        string[]  splitSentence = _intakeSentence.Split();
-        foreach (string word in splitSentence)
+        public RepeatCounter(string sentence, string word)
         {
-            if (word == _intakeWord)
-            {
-                pairs += 1;
-            }
+            _intakeSentence = sentence.ToLower();
+            _intakeWord = word.ToLower();
         }
-        return pairs;
-    }
+        public string GetSentence()
+        {
+            return _intakeSentence;
+        }
+        public string GetWord()
+        {
+            return _intakeWord;
+        }
+        public void SetSentence(string setSentence)
+        {
+            _intakeSentence = setSentence;
+        }
+        public void SetWord(string setWord)
+        {
+            _intakeWord = setWord;
+        }
+
+        public int GetPairs()
+        {
+            int pairs = 0;
+            string[] splitSentence = _intakeSentence.Split();
+            foreach (string word in splitSentence)
+            {
+                if (word == _intakeWord)
+                {
+                    pairs += 1;
+                }
+            }
+            return pairs;
+        }
 
 
     }
 
-  }
+}
